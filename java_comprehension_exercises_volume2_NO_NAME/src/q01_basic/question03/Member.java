e42a8daaa1f7712889733b726665c990e7639fc8
nahamagu <nahamagu@C010423490.zipangu.cac.co.jp>
2026-04-28 16:05:36 +0900

    0428

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/Member.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/Member.java
@@ -2,5 +2,66 @@ package q01_basic.question03;
 
 class Member {
 	//TODO ここから実装する
+	String name;
+	int age;
+	int rank;
+
+	/**
+	 * 
+	 */
+	public Member() {
+		this.name = null;
+		this.age = 0;
+		this.rank = 0;
+	}
+
+	/**
+	 * @param name
+	 * @param age
+	 * @param rank
+	 */
+	public Member(String name, int age, int rank) {
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	void showMember() {
+		if (name == null) {
+			System.out.println("***MEMBER DATA(NULL)***");
+		} else {
+			String[] parts = name.split(" ");
+			System.out.println("***MEMBER DATA(" + parts[0] + ")***");
+		}
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*****************");
+
+	}
 
 }
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -4,6 +4,15 @@ public class SystemMain03 {
 
 	public static void main(String[] args) {
 		//TODO ここから実装する
+		Member nullMember = new Member();
+		Member member = new Member();
+
+		member.setName("Miura Manabu");
+		member.setAge(28);
+		member.setRank(2);
+
+		nullMember.showMember();
+		member.showMember();
 
 	}
 
