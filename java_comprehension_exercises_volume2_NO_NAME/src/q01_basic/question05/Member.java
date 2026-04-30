0376ed71c74cf2697fdcdbabcddbd967e3a0faba
nahamagu <nahamagu@C010423490.zipangu.cac.co.jp>
2026-04-30 12:32:26 +0900

    0430　basic5まで完了しました
    オーバーライドとコンストラクタの理解が深まりました。

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/Member.java
@@ -0,0 +1,78 @@
+package q01_basic.question05;
+
+public class Member extends AbstMember {
+	int id;
+	String pasword;
+	String name;
+	int age;
+	int rank;
+
+	public Member() {
+	}
+
+	public Member(int id, String pasword, String name, int age, int rank) {
+		this.id = id;
+		this.pasword = pasword;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return pasword;
+	}
+
+	public void setPassword(String pas) {
+		this.pasword = pas;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public void buyItem() {
+		System.out.println(name + " purchased the item at 50% off");
+	}
+
+	@Override
+	public void showMember() {
+		System.out.println("---SHOW MEMBERS---");
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + id);
+		System.out.println("Password:" + pasword);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*****************");
+	}
+
+}
