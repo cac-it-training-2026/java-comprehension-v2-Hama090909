e50049014c72b797ef1c27a70b9e677f1b8a2c76
nahamagu <nahamagu@C010423490.zipangu.cac.co.jp>
2026-05-01 13:56:59 +0900

    FIX advanced question02まで完了しました。
    配列とオブジェクトの扱い、staticメソッドの使い方について理解できました。
    advanced question03は時間が足りず、行うことができませんでした。
    申し訳ございません。時間があるときに自主学習として取り組みます。

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
@@ -1,11 +1,41 @@
 package q02_advanced.question02;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SystemMain02 {
 
 	public static void main(String[] args) {
 
 		//TODO ここから実装する
+		ConsoleReader cr = new ConsoleReader();
 
+		Member m1 = Member.getInstance(1, "PasswOrd", "Miura Manabu", 28, 2);
+		Member m2 = Member.getInstance(2, "aaaAAA", "Sato Kensuke", 43, 1);
+
+		List<Member> members = new ArrayList<>();
+		members.add(m1);
+		members.add(m2);
+
+		System.out.println("===会員情報を表示します===");
+		MemberManager.showAllMembers(members);
+
+		try {
+			System.out.println("input target id>>");
+			int id = cr.inputNumber();
+
+			System.out.println("input new password>>");
+			String password = cr.inputString();
+
+			MemberManager.updatePassword(members, id, password);
+
+		} catch (IOException | NumberFormatException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("---SHOW DATA---");
+		System.out.println(members);
 	}
-
 }
